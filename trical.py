9f488e2d41d8d97834783fcca53de545bf8ac5e1
bbtg-git <darkrock.roc@gmail.com>
2025-03-06 22:37:29 +0530

    first commit

---
+++ trical.py
@@ -0,0 +1,16 @@
+print("ënter num")
+a=int(input())
+b=int(input())
+print("1.add
+       2.subtract
+       3.mul
+       4.exit")
+c=int(input())
+case c if c==1:
+	return sum(a,b)
+case c if c==2:
+	return a-b
+case c if c==3:
+	return a*b
+case c if c==4:
+	exit 0
